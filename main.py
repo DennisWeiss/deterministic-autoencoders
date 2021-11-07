61ddb83f54e8fdcd8ba436006c36bfeb18c8e5a7
Dennis Weiss <weiss.dennis98@yahoo.de>
2021-11-08 00:41:57 +0100

    Added functionality to show images

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.utils.data
 import torchvision
+import matplotlib.pyplot as plt
 
 
 train_data = torchvision.datasets.MNIST('dataset', train=True, download=True)
@@ -50,7 +51,26 @@ class RAE(nn.Module):
 
     def forward(self, x):
         z = self.encoder(x)
-        return self.decoder(z)
+        return z, self.decoder(z)
+
+
+def show_images(x, x_hat):
+    for i in range(x.shape[0]):
+        fig = plt.figure(figsize=(10, 6))
+
+        fig.add_subplot(1, 2, 1)
+
+        plt.imshow(x[i, 0, :, :])
+        plt.axis('off')
+        plt.title('x')
+
+        fig.add_subplot(1, 2, 2)
+
+        plt.imshow(x_hat.detach().numpy()[i, 0, :, :])
+        plt.axis('off')
+        plt.title('x_hat')
+
+        fig.show()
 
 
 def train_epoch(model, data_loader, optimizer):
@@ -59,7 +79,7 @@ def train_epoch(model, data_loader, optimizer):
     train_loss = 0
 
     for x, _ in data_loader:
-        x_hat = model(x)
+        z, x_hat = model(x)
         loss = ((x - x_hat)**2).sum()
 
         optimizer.zero_grad()
@@ -78,5 +98,9 @@ optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 epochs = 5
 
 for epoch in range(epochs):
+    x, _ = next(iter(train_loader))
+    z, x_hat = model(x)
+    show_images(x, x_hat)
+    print(z)
     train_loss = train_epoch(model, train_loader, optimizer)
     print('\n EPOCH {}/{} \t train loss {:.3f}'.format(epoch + 1, epochs, train_loss))
