16c44118f0b18162127686f99accdd8dc043fb99
Dennis Weiss <weiss.dennis98@yahoo.de>
2021-11-08 18:04:15 +0100

    Rendering visualization of latent features after each epoch

--- main.py
+++ main.py
@@ -5,11 +5,18 @@ import torchvision
 import matplotlib.pyplot as plt
 
 
+if torch.cuda.is_available():
+    print('GPU is available with the following device: {}'.format(torch.cuda.get_device_name()))
+else:
+    print('CPU is not available')
+
+torch.manual_seed(10)
+
 train_data = torchvision.datasets.MNIST('dataset', train=True, download=True)
 
 train_data.transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor(), torchvision.transforms.CenterCrop(30)])
 
-train_loader = torch.utils.data.DataLoader(train_data, batch_size=256)
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=256, pin_memory=True)
 
 
 class RAE(nn.Module):
@@ -60,27 +67,40 @@ def show_images(x, x_hat):
 
         fig.add_subplot(1, 2, 1)
 
-        plt.imshow(x[i, 0, :, :])
+        plt.imshow(x.cpu()[i, 0, :, :])
         plt.axis('off')
         plt.title('x')
 
         fig.add_subplot(1, 2, 2)
 
-        plt.imshow(x_hat.detach().numpy()[i, 0, :, :])
+        plt.imshow(x_hat.cpu().detach().numpy()[i, 0, :, :])
         plt.axis('off')
         plt.title('x_hat')
 
         fig.show()
 
 
-def train_epoch(model, data_loader, optimizer):
+def show_latent_features(model):
+    fig = plt.figure(figsize=(16, 16))
+    for i in range(16):
+        z = 10 * F.one_hot(torch.tensor(i), num_classes=16).float().reshape((1, 16)).to(device)
+        x_hat = model.decoder(z)
+        fig.add_subplot(4, 4, i+1)
+        plt.imshow(x_hat.cpu().detach().numpy()[0, 0, :, :])
+        plt.axis('off')
+        plt.title('latent feature {}'.format(i+1))
+    fig.show()
+
+
+def train_epoch(model, device, data_loader, optimizer):
     model.train()
 
     train_loss = 0
 
     for x, _ in data_loader:
+        x = x.to(device)
         z, x_hat = model(x)
-        loss = ((x - x_hat)**2).sum()
+        loss = ((x - x_hat).square()).sum(axis=(2, 3)).mean()
 
         optimizer.zero_grad()
         loss.backward()
@@ -92,15 +112,24 @@ def train_epoch(model, data_loader, optimizer):
     return train_loss / len(data_loader.dataset)
 
 
-model = RAE()
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+print('The model will run with {}'.format(device))
+
+model = RAE().to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 
-epochs = 5
+print('\nThe model parameters are: {}\n'.format(model.parameters))
+
+epochs = 20
 
 for epoch in range(epochs):
     x, _ = next(iter(train_loader))
+    x = x.to(device)
     z, x_hat = model(x)
-    show_images(x, x_hat)
+    # show_images(x, x_hat)
+
     print(z)
-    train_loss = train_epoch(model, train_loader, optimizer)
+    train_loss = train_epoch(model, device, train_loader, optimizer)
+    show_latent_features(model)
     print('\n EPOCH {}/{} \t train loss {:.3f}'.format(epoch + 1, epochs, train_loss))
